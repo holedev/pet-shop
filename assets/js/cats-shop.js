d2605064be52e6f5f5f404bb825ddba7d4028705
MinhTran19 <2051052081minh@ou.edu.vn>
2021-08-24 23:10:53 +0700

    commit shop cat fifth

--- assets/js/cats-shop.js
+++ assets/js/cats-shop.js
@@ -3,19 +3,281 @@ const $$ = document.querySelectorAll.bind(document)
 
 const app = {
     products: [
-
+        {
+            path: "./assets/img/cat-shop/cat_1.jpg",
+            name: "Maincoon (Female)",
+            price: "65,000,000",
+            brand: "Hà Nội"
+        },
+        {
+            path: "./assets/img/cat-shop/cat_2.jpg",
+            name: "Toyger (Blue - Female)",
+            price: "420,500,000",
+            brand: "Nga"
+        },
+        {
+            path: "./assets/img/cat-shop/cat_3.jpg",
+            name: "Bengal Munchkin (Female)",
+            price: "470,000,000",
+            brand: "Nga"
+        },
+        {
+            path: "./assets/img/cat-shop/cat_4.jpg",
+            name: "Toyger (White - Male)",
+            price: "380,700,000",
+            brand: "Nga"
+        },
+        {
+            path: "./assets/img/cat-shop/cat_5.jpg",
+            name: "Toyger (Female)",
+            price: "411,000,000",
+            brand: "Nga"
+        },
+        {
+            path: "./assets/img/cat-shop/cat_6.jpg",
+            name: "Golden (NY12 - Female)",
+            price: "280,000,000",
+            brand: "Anh"
+        },
+        {
+            path: "./assets/img/cat-shop/cat_7.jpg",
+            name: "Maincoon (Male)",
+            price: "100,000,000",
+            brand: "Anh"
+        },
+        {
+            path: "./assets/img/cat-shop/cat_8.jpg",
+            name: "Bengal Munchkin (Male)",
+            price: "430,000,000",
+            brand: "Nga"
+        },
+        {
+            path: "./assets/img/cat-shop/cat_9.jpg",
+            name: "Golden (longhair - Male)",
+            price: "60,000,000",
+            brand: "Hà Nội"
+        },
+        {
+            path: "./assets/img/cat-shop/cat_10.jpg",
+            name: "Golden (NY12 - Female)",
+            price: "238,000,000",
+            brand: "Hồ Chí Minh"
+        },
+        {
+            path: "./assets/img/cat-shop/cat_11.jpg",
+            name: "Scottish (AY25 - Male)",
+            price: "334,200,000",
+            brand: "Nga"
+        },
+        {
+            path: "./assets/img/cat-shop/cat_12.jpg",
+            name: "Miracle (BY11 - Male)",
+            price: "80,000,000",
+            brand: "Nga"
+        },
+        {
+            path: "./assets/img/cat-shop/cat_13.jpg",
+            name: "Miracle (NY11 - Female)",
+            price: "65,000,000",
+            brand: "Hà Nội"
+        },
+        {
+            path: "./assets/img/cat-shop/cat_14.jpg",
+            name: "Miracle (Male)",
+            price: "227,836,000",
+            brand: "Hà Nội"
+        },
+        {
+            path: "./assets/img/cat-shop/cat_15.jpg",
+            name: "Golden (NY12 - Male)",
+            price: "132,702,000",
+            brand: "Mỹ"
+        },
+        {
+            path: "./assets/img/cat-shop/cat_16.jpg",
+            name: "Toyger (Male)",
+            price: "350,650,000",
+            brand: "Nga"
+        },
+        {
+            path: "./assets/img/cat-shop/cat_17.jpg",
+            name: "Miracle (Female)",
+            price: "182,000,000",
+            brand: "Nga"
+        },
+        {
+            path: "./assets/img/cat-shop/cat_18.jpg",
+            name: "Golden (NY1222 - Female)",
+            price: "285,000,000",
+            brand: "Nga"
+        },
+        {
+            path: "./assets/img/cat-shop/cat_19.jpg",
+            name: "Munchkin (Silver - Male)",
+            price: "106,030,000",
+            brand: "Nga"
+        },
+        {
+            path: "./assets/img/cat-shop/cat_20.jpg",
+            name: "Golden (longhair - Female)",
+            price: "90,000,000",
+            brand: "Anh"
+        },
+        {
+            path: "./assets/img/cat-shop/cat_21.jpg",
+            name: "Golden (NY12 - Female)",
+            price: "227,969,630",
+            brand: "Mỹ"
+        },
+        {
+            path: "./assets/img/cat-shop/cat_22.jpg",
+            name: "Anh lông ngắn (Lilac - Male)",
+            price: "40,000,000",
+            brand: "Hồ Chí Minh"
+        },
+        {
+            path: "./assets/img/cat-shop/cat_23.jpg",
+            name: "Golden (longhair - Female)",
+            price: "158,000,000",
+            brand: "Nga"
+        },
+        {
+            path: "./assets/img/cat-shop/cat_24.jpg",
+            name: "Golden (Male)",
+            price: "100,000,000",
+            brand: "Nga"
+        },
+        {
+            path: "./assets/img/cat-shop/cat_25.jpg",
+            name: "Golden (BY12 - Male)",
+            price: "150,000,000",
+            brand: "Nga"
+        },
+        {
+            path: "./assets/img/cat-shop/cat_26.jpg",
+            name: "Toyger (Female)",
+            price: "402,000,000",
+            brand: "Anh"
+        },
+        {
+            path: "./assets/img/cat-shop/cat_27.jpg",
+            name: "Anh lông ngắn (Xám xanh - Female)",
+            price: "30,000,000",
+            brand: "Bến Tre"
+        },
+        {
+            path: "./assets/img/cat-shop/cat_28.jpg",
+            name: "Golden (Light - Male)",
+            price: "150,000,000",
+            brand: "Nga"
+        },
+        {
+            path: "./assets/img/cat-shop/cat_29.jpg",
+            name: "Maincoon (Male)",
+            price: "110,000,000",
+            brand: "Nga"
+        },
+        {
+            path: "./assets/img/cat-shop/cat_30.jpg",
+            name: "Golden (NY12/24 - Female)",
+            price: "144,570,000",
+            brand: "Nga"
+        },
+        {
+            path: "./assets/img/cat-shop/cat_31.jpg",
+            name: "Miracle (NY11/12 - Male)",
+            price: "114,000,000",
+            brand: "Nga"
+        },
+        {
+            path: "./assets/img/cat-shop/cat_32.jpg",
+            name: "Golden (longhair - Male)",
+            price: "105,070,000",
+            brand: "Anh"
+        },
+        {
+            path: "./assets/img/cat-shop/cat_33.jpg",
+            name: "Munchkin (Male)",
+            price: "80,000,000",
+            brand: "Hà Nội"
+        },
+        {
+            path: "./assets/img/cat-shop/cat_34.jpg",
+            name: "Munchkin (NY11 - Male)",
+            price: "123,423,000",
+            brand: "Nga"
+        },
+        {
+            path: "./assets/img/cat-shop/cat_35.jpg",
+            name: "Sphyxn (Male)",
+            price: "80,000,000",
+            brand: "Hồ Chí Minh"
+        },
+        {
+            path: "./assets/img/cat-shop/cat_36.jpg",
+            name: "Golden (NY12 - Male)",
+            price: "135,806,000",
+            brand: "Nga"
+        },
+        {
+            path: "./assets/img/cat-shop/cat_37.jpg",
+            name: "Munchkin (Cinnamon - Male)",
+            price: "95,342,000",
+            brand: "Hồ Chí Minh"
+        },
+        {
+            path: "./assets/img/cat-shop/cat_38.jpg",
+            name: "Munchkin (Male)",
+            price: "75,000,000",
+            brand: "Hà Nội"
+        },
+        {
+            path: "./assets/img/cat-shop/cat_39.jpg",
+            name: "Munchkin (CY12 - Female)",
+            price: "121,070,000",
+            brand: "Hà Nội"
+        },
+        {
+            path: "./assets/img/cat-shop/cat_40.jpg",
+            name: "Miracle (Male)",
+            price: "150,000,000",
+            brand: "Trung Quốc"
+        },
     ],
     render: function() {
-        const htmls = this.products.map(function(product, index){
-            return ``
+        const htmls = this.products.map(function(product){
+            return `
+            <div class="main-content-classify__pet-item-member">
+                <div class="main-content-classify__pet-item-member__img">
+                    <img src="${product.path}" alt="" width="100%">
+                    <div class="main-content-classify__pet-item-member__img-around">
+                        <div class="main-content-classify__pet-item-member__img-button">
+                            Chốt Đơn
+                        </div>
+                    </div>
+                </div>
+                <div class="main-content-classify__pet-item-member__name">
+                    <h4>${product.name}</h4>
+                    <h5>(Pedigreed Cats)</h5>
+                </div>
+                <div class="main-content-classify__pet-item-member-price">
+                    <span class="main-content-classify__pet-item-member-price__value">${product.price}</span>&#8363;
+                </div>
+                <div class="main-content-classify__pet-item-member-brand">
+                    <small>Xuất xứ: <span>${product.brand}</span></small>
+                </div>
+            </div>
+            `
         })
-        const x = htmls.toString()
-        console.log(x)
+        $('.main-content-classify__pet-item').innerHTML = htmls.join('')
+        this.products.filter((product) => {
+            return product.new
+        })
     },
     handleEvent: function() {
 
     },
-    start: function () {
+    start: function() {
         this.render()
         this.handleEvent()
     }
