e210334862c955f1de49b946349cad0a371df90d
MinhTran19 <2051052081minh@ou.edu.vn>
2021-09-26 17:26:51 +0700

    commit scroll page Cat Shop

---
+++ assets/js/Page_Dog.js
@@ -0,0 +1,12 @@
+// Ham scroll fix bug Header che khuat
+$(document).ready(function() {
+    $("#one").click(function() {
+        const h1 = $(this).attr('href');
+        const t1 = $(h1).prop("offsetTop");
+        console.log(t1);
+        $("html, body").animate({
+            scrollTop: (t1 - 68)
+        })
+    })
+    
+})
--- assets/js/commentCatShop.js
+++ assets/js/commentCatShop.js
@@ -18,19 +18,51 @@ $(document).ready(function() {
 
 // Ham scroll fix bug Header che khuat
 $(document).ready(function() {
-    const headerHeight = 200
+    const headerHeight = $(".header-tab").height()
+
     $("#pets").click(function() {
+        const hPet = $(this).attr('href')
+        const tPet = $(hPet).prop("offsetTop")
+        
         $("html, body").animate({
-            scrollTop: 0
-        })   
+            scrollTop: (tPet - headerHeight)
+        })  
     })
 
     $("#stuffs").click(function() {
-        const h = $(this).attr('href')
-        const t = $(h).prop("offsetTop")
+        const hStuff = $(this).attr('href')
+        const tStuff = $(hStuff).prop("offsetTop")
+        
         $("html, body").animate({
-            scrollTop: (t - headerHeight)
-        }, 2000)
+            scrollTop: (tStuff - headerHeight)
+        })
+    })
+
+    $("#medicines").click(function() {
+        const hMedi = $(this).attr('href')
+        const tMedi = $(hMedi).prop("offsetTop")
+        
+        $("html, body").animate({
+            scrollTop: (tMedi - headerHeight)
+        })
+    })
+
+    $("#buys").click(function() {
+        const hBuy = $(this).attr('href')
+        const tBuy = $(hBuy).prop("offsetTop")
+        
+        $("html, body").animate({
+            scrollTop: (tBuy - headerHeight)
+        })
+    })
+
+    $("#comments").click(function() {
+        const hCmt = $(this).attr('href')
+        const tCmt = $(hCmt).prop("offsetTop")
+        
+        $("html, body").animate({
+            scrollTop: (tCmt - headerHeight)
+        })
     })
 })
 
@@ -38,14 +70,16 @@ $(document).ready(function() {
 $(document).ready(function() {
     $("#add-item").click(function() {
         var content = $("#add-comment").val()
+        var name = $("#add-name").val()
 
         $("div.ctrl").prepend(`
             <div class="item active-comment">
                 <div class="avatar col10">
-                    <img src="./assets/img/avtar.png" alt="avatar" width="50%">
+                    <img src="./assets/img/avtar.png" alt="avatar" width="100%">
                 </div>
                 <div class="content-comment col80">
-                    ${content}
+                    <div class="name_User">${name}</div>
+                    <div class="content-comment-text">${content}</div>
                 </div>
                 <div class="delete col10">
                     <input type="button" name="delete" value="Xóa">
@@ -59,6 +93,37 @@ $(document).ready(function() {
     })
 
     $("div.ctrl").on("click", "div.item input[type=button]", function() {
+        if (confirm("Bạn có muốn xóa dòng bình luận này?") == true)
+            $(this).parent().parent().parent().remove()
+    })
+})
+
+$(document).ready(function() {
+    $("#add-item-reply").click(function() {
+        var contentReply = $("#add-comment-reply").val()
+        var nameReply = $("#add-name-reply").val()
+
+        $("div.item-reply").append(`
+            <div class="content-comment_reply active-comment">
+                <div class="avatar col10">
+                    <img src="./assets/img/avtar.png" alt="avatar" width="100%">
+                </div>
+                <div class="content-comment col80">
+                    <div class="name_User">${nameReply}</div>
+                    <div class="content-comment-text">${contentReply}</div>
+                </div>
+                <div class="delete col10">
+                    <input type="button" name="delete" value="Xóa">
+                </div>
+            </div>
+        `)
+
+        setTimeout(function() {
+            $("div.content-comment_reply").removeClass("active-comment")
+        }, 1000)
+    })
+
+    $("div.item-reply").on("click", "div.content-comment_reply input[type=button]", function() {
         if (confirm("Bạn có muốn xóa dòng bình luận này?") == true)
             $(this).parent().parent().remove()
     })
